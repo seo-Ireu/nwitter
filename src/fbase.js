56cb11d4cfbdb6e0546d51ca28ca3b718cb74109
limsohyun <ssoms_@naver.com>
2021-10-15 17:44:57 +0900

    c5. file Attribute Add

--- src/fbase.js
+++ src/fbase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
+import {getStorage} from "firebase/storage";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +23,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const dbService = getFirestore();
 export const authService = getAuth();
+export const storageService = getStorage(app);
